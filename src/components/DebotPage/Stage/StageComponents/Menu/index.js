6a06eae9a8ef7564b6005c289f6345e9b1d144ed
Anton Solodkov <anton.solodkov@sharekey.com>
2021-04-20 23:31:45 +0300

    add logic to hide/show menu items

--- src/components/DebotPage/Stage/StageComponents/Menu/index.js
+++ src/components/DebotPage/Stage/StageComponents/Menu/index.js
@@ -1,37 +1,71 @@
-import React, { useState } from 'react';
+import React, { useState, useLayoutEffect, useCallback } from 'react';
 import { useDebotAddress } from 'src/helpers';
 import { DEngine } from 'src/debot';
 import './index.scss';
 
+const MAX_ITEMS = 7;
+
 const Menu = ({ params }) => {
 	const debotAddress = useDebotAddress();
 	const [errorText, setErrorText] = useState('');
+	const [menuItemsToRender, setMenuItemsToRender] = useState(false);
+	const [isShowAllMenuItems, setIsShowAllMenuItems] = useState(false);
 	const { text, title, interfaceAddress, menuItems } = params;
 
-	const handleSelectItem = async (selectedItem, index) => {
+	const handleSelectItem = useCallback(async (selectedItem, index) => {
 		try {
 			await DEngine.callDebotFunction(debotAddress, interfaceAddress, selectedItem.functionId, { index });
 		} catch(err) {
 			setErrorText(err.message);
 		}
-	}
+	}, [debotAddress, interfaceAddress]);
 
-	const menuItemsToRender = menuItems.map((item, index) => {
-		let menuName = item.title;
+	useLayoutEffect(() => {
+		let itemsToRender = menuItems.map((item, index) => {
+			let menuName = item.title;
+	
+			if (item.description)
+				menuName += ` (${item.description})`;
+	
+			return (
+				<div
+					onClick={() => handleSelectItem(item, index)}
+					key={`${item.functionId}-${index}`}
+					className='stage-component__menu-item'
+				>
+					{menuName}
+				</div>
+			);
+		});
 
-		if (item.description)
-			menuName += ` ${item.description}`;
+		if (itemsToRender.length > MAX_ITEMS) {
+			if (isShowAllMenuItems) {
+				itemsToRender.push(
+					<div
+						onClick={() => setIsShowAllMenuItems(false)}
+						key='hide-btn'
+						className='stage-component__menu-item'
+					>
+						{'Hide'}
+					</div>
+				)
+			} else {
+				itemsToRender = itemsToRender.slice(0, MAX_ITEMS);
 
-		return (
-			<div
-				onClick={() => handleSelectItem(item, index)}
-				key={`${item.functionId}-${index}`}
-				className='stage-component__menu-item'
-			>
-				{menuName}
-			</div>
-		);
-	});
+				itemsToRender.push(
+					<div
+						onClick={() => setIsShowAllMenuItems(true)}
+						key='show-more-btn'
+						className='stage-component__menu-item'
+					>
+						{'Show more'}
+					</div>
+				)
+			}
+		}
+
+		setMenuItemsToRender(itemsToRender);
+	}, [menuItems, handleSelectItem, isShowAllMenuItems]);
 
 	return (
 		<div className='stage-component__menu-container'>
