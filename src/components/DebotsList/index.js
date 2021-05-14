967b8addd3322e1e2d8fb8dd3e0d4734a9ab9666
Anton Solodkov <anton.solodkov@sharekey.com>
2021-05-15 02:32:44 +0300

    update style for list

--- src/components/DebotsList/index.js
+++ src/components/DebotsList/index.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react';
 import { useSelector } from 'react-redux';
 import { Loader, ControlWithPopup, AddDebot, Environment } from 'src/components';
+import { isCustomScrollBar } from 'src/helpers';
 import DebotListItem from './ListItem';
 import './index.scss';
 
@@ -26,6 +27,7 @@ const DebotsList = () => {
 			key={`${debot.address}-${index}`}
 		/>
 	));
+	const debotsListClassName = `debots-list__list ${isCustomScrollBar() ? 'with-custom-scrollbar' : ''}`;
 
 	return (
 		<div className='debots-list'>
@@ -37,19 +39,21 @@ const DebotsList = () => {
 					<Environment />
 				</ControlWithPopup>
 			</div>
-			{!!filteredLocalDebots.length
-				&& <Fragment>
-					<div className='debots-list__section-title'>Your saved DeBots</div>
-					{renderLocalDebotListItems()}
-				</Fragment>
-			}
-			{!!filteredDebots.length
-				&& <Fragment>
-					<div className='debots-list__section-title'>Suggested DeBots</div>
-					{renderDebotListItems()}
-				</Fragment>
-			}
-			{(!filteredDebots.length && !filteredLocalDebots.length) && <Loader isFailed />}
+			<div className={debotsListClassName}>
+				{!!filteredLocalDebots.length
+					&& <Fragment>
+						<div className='debots-list__section-title'>Your saved DeBots</div>
+						{renderLocalDebotListItems()}
+					</Fragment>
+				}
+				{!!filteredDebots.length
+					&& <Fragment>
+						<div className='debots-list__section-title'>Suggested DeBots</div>
+						{renderDebotListItems()}
+					</Fragment>
+				}
+				{(!filteredDebots.length && !filteredLocalDebots.length) && <Loader isFailed />}
+			</div>
 		</div>
 	);
 }
